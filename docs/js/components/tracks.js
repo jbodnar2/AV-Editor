1b6d3e01c0b88956af21a6db0047f49527d0183a
Jon Bodnar <jbodnar2@gsu.edu>
2024-08-04 10:32:29 -0400

    Fix Chrome caption, controls overlap

--- docs/js/components/tracks.js
+++ docs/js/components/tracks.js
@@ -98,6 +98,8 @@ function addTrack(videoElement, transcript) {
     cue.id = id;
     cue.startTimeFormatted = formatTime(start);
     cue.endTimeFormatted = formatTime(end);
+    // Avoid Chrome cue / controls overlap--set cue display to top of video.
+    cue.line = 1;
 
     // If the segment has words, calculate and set the probabilities of the words
     if (words) {
