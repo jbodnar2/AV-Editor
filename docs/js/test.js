816ad6f9d1fd97616b1db6f2772697c8da165289
Jon Bodnar <jbodnar2@gsu.edu>
2024-08-10 06:33:30 -0400

    Update test code for selection

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,17 +1,55 @@
+/**
+ *
+ */
+
+// ----- Variables ----- //
+
+// Get the media element and update its attributes
+// - Using Object.assign() for setting attributes only because I find it easier
+//   to reach when setting multiple attributes on an aelement
 const mediaElement = document.querySelector("video");
 Object.assign(mediaElement, {
   controls: true,
-  autoplay: "true",
-  muted: "true",
+  // autoplay: true,
+  muted: true,
   loop: true,
   poster: "media/images/01.jpeg",
   src: "media/audio-video/newscast.mp4",
-  playbackRate: "2.5",
+  playbackRate: "4.0",
 });
 
+// Get the wrapping element into which I'll render the captions
 const captionsElement = document.querySelector("#captions");
+
+// Create a map for the cues (keys) and (cueElements) values.
+// - Using a map because keys/values can be anything. Key and values will be
+//  "live" elements so updating one will update the element or values and
+//  consequently update the UI
 const cuesMap = new Map();
 
+// ----- Functions ----- //
+
+function findKeyByValue(map, value) {
+  if (!map || !value) return null;
+
+  // More efficient?
+  return Array.from(map.entries()).find(([key, val]) => val === value)?.[0] || null;
+
+  // Not sure if the above code, below code, or using Map.prototype.forEach()
+  // ...would be more efficient? Better to create a function or not? Will I ever
+  // ..perform this operation more than onece?
+
+  // for (let [key, val] of map) {
+  //   if (val === value) {
+  //     return key;
+  //   }
+  // }
+  // return null;
+}
+
+// ----- Initial setup & Listeners ----- //
+
+//
 mediaElement.addEventListener("loadeddata", event => {
   const textTracks = mediaElement.textTracks;
   if (!textTracks) return;
@@ -46,6 +84,17 @@ mediaElement.addEventListener("loadeddata", event => {
       };
     }
   }
+});
+
+document.addEventListener("click", event => {
+  const cueElement = event.target;
+  if (!cueElement.matches('[id^="cue-"]')) return;
+
+  // const cue = findKeyByValue(cuesMap, cueElement);
+  const cue = Array.from(cuesMap.entries()).find(([key, val]) => val === cueElement)?.[0] || null;
+  if (!cue) return;
+
+  mediaElement.currentTime = cue.startTime;
 });
 
 // textTracks.onchange = () => {
