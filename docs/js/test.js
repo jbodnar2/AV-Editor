19080f5b6ab0894991e4e1dada78eb9034aa58aa
Jon Bodnar <jbodnar2@gsu.edu>
2024-08-11 10:07:43 -0400

    Remove unnecessary

--- docs/js/test.js
+++ docs/js/test.js
@@ -76,40 +76,41 @@ function findKeyByValue(map, value) {
 
 // ----- Initial setup & Listeners ----- //
 
-//
 mediaElement.addEventListener("loadeddata", event => {
   const textTracks = mediaElement.textTracks;
+
   if (!textTracks) return;
 
-  for (const track of textTracks) {
-    if (!track.cues) return;
+  for (const textTrack of textTracks) {
+    if (!textTrack.cues) continue;
 
-    let count = 0;
-    for (const cue of track.cues) {
+    let cueCount = 0;
+
+    for (const cue of textTrack.cues) {
       const cueElement = document.createElement("div");
-      Object.assign(cueElement, {
-        id: `cue-${count++}`,
-        textContent: cue.text,
-      });
+      cue.id = cueCount;
+      Object.assign(cueElement, { id: `cue-${cueCount++}`, textContent: cue.text });
 
       cuesMap.set(cue, cueElement);
-
-      const frag = document.createDocumentFragment();
-
-      cuesMap.forEach(entry => frag.append(entry));
-      captionsElement.append(frag);
-
-      cue.onenter = event => {
-        const cue = cuesMap.get(event.target);
-        cue.scrollIntoView({ behavior: "smooth", block: "center" });
-        cue.classList.add("active");
-      };
-
-      cue.onexit = event => {
-        const cue = cuesMap.get(event.target);
-        cue.classList.remove("active");
-      };
     }
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  cuesMap.forEach((cueElement, cue) => fragment.append(cueElement));
+  captionsElement.append(fragment);
+
+  for (const cue of cuesMap.keys()) {
+    cue.onenter = event => {
+      const associatedCueElement = cuesMap.get(event.target);
+      associatedCueElement.scrollIntoView({ behavior: "smooth", block: "center" });
+      associatedCueElement.classList.add("active");
+    };
+
+    cue.onexit = event => {
+      const associatedCueElement = cuesMap.get(event.target);
+      associatedCueElement.classList.remove("active");
+    };
   }
 });
 
@@ -122,35 +123,3 @@ document.addEventListener("click", event => {
 
   mediaElement.currentTime = cue.startTime;
 });
-
-// textTracks.onchange = () => {
-//   for (const track of textTracks) {
-//     track.oncuechange = event => {
-//       const activeCue = event.target?.activeCues[0];
-//       const cueText = activeCue?.text;
-
-//       if (!cueText) return;
-
-//       const cueElement = cues.get(activeCue) ?? document.createElement("div");
-//       cueElement.textContent = cueText;
-//       cueElement.id = `cue-${activeCue.id}`;
-//       cueElement.className = "line";
-//       cues.set(activeCue, cueElement);
-
-//       captionsElement.append(cueElement);
-
-//       if (cueElement.nextElementSibling) {
-//         cueElement.nextElementSibling.classList.remove("active");
-//       }
-
-//       cueElement.classList.add("active");
-
-//       const firstChild = captionsElement.firstElementChild;
-//       if (firstChild && firstChild !== cueElement) {
-//         captionsElement.removeChild(firstChild);
-//       }
-
-//       cueElement.scrollIntoView({ behavior: "smooth", block: "end" });
-//     };
-//   }
-// };
