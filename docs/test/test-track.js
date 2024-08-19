2904979bed8a6a02768ffdf22c9a004a18f1d133
Jon Bodnar <jbodnar2@gsu.edu>
2024-08-19 06:06:25 -0400

    Add note

--- docs/test/test-track.js
+++ docs/test/test-track.js
@@ -2,6 +2,8 @@ function addVideoTrackAndCues(videoElement, mediaData) {
   if (!videoElement) return;
   if (!mediaData) return;
 
+  // TODO: Investigate track.kind options and how it might affect, loading, downloads, etc
+  // NOTE: Default is metadata, c.f. https://developer.mozilla.org/en-US/docs/Web/HTML/Element/track
   const trackData = mediaData.track;
   const track = videoElement.addTextTrack(trackData.kind, trackData.label, trackData.srclang);
   track.mode = "showing";
